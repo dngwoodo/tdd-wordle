035eba52a4d5401f2ad0073c24dae6b8fe99bfe3
HyeongTak Shin <77006427+dodoongtak@users.noreply.github.com>
2022-05-13 19:06:07 +0800

    findGrayposition구현 리팩터링

--- Game.js
+++ Game.js
@@ -50,41 +50,16 @@ class Game {
     return positions.filter((p) => answerLetters.includes(guessLetters[p]));
   }
 
-  /**
- * Goal: 모든 index의 포지션을 돌려준다.
- *
- * 1. guess와 answer가 필요하다.
- * 2. guess의 하나하나의 레터가 answer에 있는지를 확인한다.
- *    -
- *
- * 갓챠: 반복은 먼저 찾자.
- */
-
-  // eslint-disable-next-line class-methods-use-this
   findGrayPositionsFor(guess) {
     const guessLetters = guess.getLetters();
     const answerLetters = this.answer.getLetters();
 
     const result = [];
 
-    if (guessLetters.includes(answerLetters[0]) === false) {
-      result.push(0);
-    }
-
-    if (guessLetters.includes(answerLetters[1]) === false) {
-      result.push(1);
-    }
-
-    if (guessLetters.includes(answerLetters[2]) === false) {
-      result.push(2);
-    }
-
-    if (guessLetters.includes(answerLetters[3]) === false) {
-      result.push(3);
-    }
-
-    if (guessLetters.includes(answerLetters[4]) === false) {
-      result.push(4);
+    for (let i = 0; i < 5; i += 1) {
+      if (guessLetters.includes(answerLetters[i]) === false) {
+        result.push(i);
+      }
     }
 
     return result;
